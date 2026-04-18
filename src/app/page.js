2256e0f1fffa7f413757856b94311bdb82a124e0
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-04-18 21:24:49 +0100

    chore: remove extra signin sign up buttons.

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,6 @@ import HomeChart from '@/components/HomeChart';
 export default function Home() {
   return (
     <>
-      <div style={{ display: "flex", gap: "1rem" }}>
-        <SignInButton mode="modal">
-          <button className="btn-primary">Sign In</button>
-        </SignInButton>
-
-        <SignUpButton mode="modal">
-          <button className="btn-secondary">Sign Up</button>
-        </SignUpButton>
-      </div>
       <HomeChart />
     </>
   );
